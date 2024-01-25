abfac4c482bbd24be2ec9a715f247297ad16ab6f
Kwabena Boakye <kwaboakye93@gmail.com>
2024-01-25 22:09:59 +0000

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -22,6 +22,5 @@ def rotate_2d_matrix(matrix):
     for i in range(n):
         for j in range(int(n / 2)):
             temp = matrix[i][j]
-            matrix[i][j] = matrix[i][n-1-j]
-            matrix[i][n-1-j] = temp
-            
+            matrix[i][j] = matrix[i][n - 1 - j]
+            matrix[i][n - 1 - j] = temp
